e9a5c62775b352a89b1f8372b8b56237aa6c4dce
Renan Almeida <renan.almeida@solidify.dev>
2024-11-05 16:12:18 +0100

    WIP Next button

--- pages/first_page.py
+++ pages/first_page.py
@@ -11,5 +11,12 @@ class FirstPage:
     def input_token(self, token):
         wait = WebDriverWait(self.driver, 10)
         token_input = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Auth Token"]')))
-        self.driver.execute_script("arguments[0].value = arguments[1];", token_input, token)
+        
+        # Use JavaScript to set the value and trigger the input event
+        self.driver.execute_script("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('input'));", token_input, token)
+        
+        # Verify the value of the input field
+        value = self.driver.execute_script("return arguments[0].value;", token_input)
+        print(f"Token input value: {value}")
+        
         token_input.send_keys(Keys.RETURN)
